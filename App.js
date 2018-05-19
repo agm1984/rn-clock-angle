0c88d5bca2c773e6918df15e11a9539b20f7a50a
Adam Mackintosh <agm1984@gmail.com>
2018-05-19 13:33:03 -0700

    unit test layer setup and functioning

--- App.js
+++ App.js
@@ -1,23 +1,87 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import {
+  View, Text, TextInput, StyleSheet,
+} from 'react-native'
+import Clock from './src/components/Clock/Clock'
 
-export default class App extends React.Component {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentHour: '2',
+      currentMinute: '30',
+    }
+  }
+
   render() {
+    const { currentHour, currentMinute } = this.state
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={styles.appWrapper}>
+        <View style={styles.appContainer}>
+          <Text style={styles.appHeading}>
+            [%] INANGLES
+          </Text>
+          <View style={styles.currentTimeWrapper}>
+            <TextInput
+              style={styles.hoursInput}
+              placeholder={currentHourz}
+              onChangeText={currentHour => this.setState({ currentHour })}
+              value=""
+            />
+            <TextInput
+              style={styles.minuteInput}
+              placeholder={currentMinute}
+              onChangeText={currentMinute => this.setState({ currentMinute })}
+              value=""
+            />
+          </View>
+          <Clock
+            minuteHandLength={110}
+          />
+        </View>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appWrapper: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
-});
+  appContainer: {
+    alignItems: 'stretch',
+  },
+  appHeading: {
+    backgroundColor: '#000',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 32,
+  },
+  currentTimeWrapper: {
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: 'brown',
+  },
+  hoursInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    textAlign: 'center',
+    padding: 10,
+    margin: 10,
+  },
+  minuteInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    textAlign: 'center',
+    padding: 10,
+    margin: 10,
+  }
+})
+
+export default App
--- App.test.js
+++ App.test.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import App from './App';
+import React from 'react'
+import App from './App'
+//import renderer from 'react-test-renderer'
+import renderer from 'react-native-test-utils'
 
-import renderer from 'react-test-renderer';
+describe('./App.js', () => {
+  const app = renderer(<App />)
+  const title = '[%] INANGLES'
+  const currentHour = '2'
+  const currentMinute = '30'
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
-});
+  it('renders without crashing', () => {
+    expect(app).toMatchSnapshot()
+  })
+
+  it('has the correct title', () => {
+    let appHeading = app.query('Text')
+    expect(appHeading.text()).toEqual(title)
+  })
+
+  describe('Hour Input', () => {
+    it('displays current hour as placeholder', () => {
+      let textInputView = app.query("TextInput[placeholder='2']")
+      textInputView.simulate('changeText', 'react-native-test-utils')
+    })
+  })
+
+  describe('Minute Input', () => {
+    it('displays current minute as placeholder', () => {
+      let textInputView = app.query("TextInput[placeholder='30']")
+      textInputView.simulate('changeText', 'react-native-test-utils')
+    })
+  })
+})
